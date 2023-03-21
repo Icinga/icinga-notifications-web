dd2fc32c4fe8a2b97200f7ade81d3db7cb9fc2ad
Sukhwinder Dhillon <sukhwinder33445@gmail.com>
2023-03-21 16:54:11 +0100

    Introduce `LoadMore`

--- library/Noma/Widget/ItemList/EventList.php
+++ library/Noma/Widget/ItemList/EventList.php
@@ -4,11 +4,28 @@
 
 namespace Icinga\Module\Noma\Widget\ItemList;
 
+use Icinga\Module\Noma\Common\LoadMore;
 use Icinga\Module\Noma\Common\BaseItemList;
+use ipl\Orm\ResultSet;
 
 class EventList extends BaseItemList
 {
+    use LoadMore;
+
     protected $defaultAttributes = ['class' => 'event-list'];
+
+    /** @var ResultSet */
+    protected $data;
+
+    public function __construct(ResultSet $data)
+    {
+        parent::__construct($data);
+    }
+
+    protected function init(): void
+    {
+        $this->data = $this->getIterator($this->data);
+    }
 
     protected function getItemClass(): string
     {
---
+++ library/Noma/Widget/ShowMore.php
@@ -0,0 +1,61 @@
+<?php
+
+/* Icinga NoMa Web | (c) 2023 Icinga GmbH | GPLv2 */
+
+namespace Icinga\Module\Noma\Widget;
+
+use ipl\Html\BaseHtmlElement;
+use ipl\Orm\ResultSet;
+use ipl\Web\Common\BaseTarget;
+use ipl\Web\Url;
+use ipl\Web\Widget\ActionLink;
+
+class ShowMore extends BaseHtmlElement
+{
+    use BaseTarget;
+
+    protected $defaultAttributes = ['class' => 'show-more'];
+
+    protected $tag = 'div';
+
+    protected $resultSet;
+
+    protected $url;
+
+    protected $label;
+
+    public function __construct(ResultSet $resultSet, Url $url, string $label = null)
+    {
+        $this->label = $label;
+        $this->resultSet = $resultSet;
+        $this->url = $url;
+    }
+
+    public function setLabel(string $label): self
+    {
+        $this->label = $label;
+
+        return $this;
+    }
+
+    public function getLabel(): string
+    {
+        return $this->label ?: t('Show More');
+    }
+
+    public function renderUnwrapped(): string
+    {
+        if ($this->resultSet->hasMore()) {
+            return parent::renderUnwrapped();
+        }
+
+        return '';
+    }
+
+    protected function assemble()
+    {
+        if ($this->resultSet->hasMore()) {
+            $this->add(new ActionLink($this->getLabel(), $this->url));
+        }
+    }
+}
