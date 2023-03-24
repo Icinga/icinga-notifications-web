5c4149a376a7b70156b8fd1342cdbbc74639faac
raviks789 <33730024+raviks789@users.noreply.github.com>
2023-03-24 13:08:42 +0100

    Create a list of items for contacts in the database

--- configuration.php
+++ configuration.php
@@ -10,3 +10,13 @@ $this->provideConfigTab(
         'url'   => 'config/database'
     ]
 );
+
+$cssDirectory = $this->getCssDir();
+$cssFiles = new RecursiveIteratorIterator(new RecursiveDirectoryIterator(
+    $cssDirectory,
+    RecursiveDirectoryIterator::CURRENT_AS_PATHNAME | RecursiveDirectoryIterator::SKIP_DOTS
+));
+
+foreach ($cssFiles as $path) {
+    $this->provideCssFile(ltrim(substr($path, strlen($cssDirectory)), DIRECTORY_SEPARATOR));
+}
---
+++ library/Noma/Widget/EmptyState.php
@@ -0,0 +1,27 @@
+<?php
+
+/* Icinga NoMa Web | (c) 2023 Icinga GmbH | GPLv2 */
+
+namespace Icinga\Module\Noma\Widget;
+
+use ipl\Html\BaseHtmlElement;
+
+class EmptyState extends BaseHtmlElement
+{
+    /** @var mixed Content */
+    protected $content;
+
+    protected $tag = 'div';
+
+    protected $defaultAttributes = ['class' => 'empty-state'];
+
+    public function __construct($content)
+    {
+        $this->content = $content;
+    }
+
+    protected function assemble()
+    {
+        $this->add($this->content);
+    }
+}
