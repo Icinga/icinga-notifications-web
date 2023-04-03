ddcdb849854b6982b6a33e45962c4f6383d4825f
raviks789 <33730024+raviks789@users.noreply.github.com>
2023-04-03 10:40:24 +0200

    Add `Channel` model for `channel` table

---
+++ library/Noma/Model/Channel.php
@@ -0,0 +1,48 @@
+<?php
+
+/* Icinga NoMa Web | (c) 2023 Icinga GmbH | GPLv2 */
+
+namespace Icinga\Module\Noma\Model;
+
+use ipl\Orm\Model;
+
+class Channel extends Model
+{
+    public function getTableName(): string
+    {
+        return 'channel';
+    }
+
+    public function getKeyName(): string
+    {
+        return 'id';
+    }
+
+    public function getColumns(): array
+    {
+        return [
+            'name',
+            'type',
+            'config'
+        ];
+    }
+
+    public function getColumnDefinitions()
+    {
+        return [
+            'name'   => t('Name'),
+            'type'   => t('Type'),
+        ];
+    }
+
+    public function getSearchColumns()
+    {
+        return ['name'];
+    }
+
+
+    public function getDefaultSort()
+    {
+        return ['name'];
+    }
+}
