28e19d7e7e3a4f90a1a0b6c7eba4379c18f8b246
raviks789 <33730024+raviks789@users.noreply.github.com>
2023-04-03 10:48:25 +0200

    Show channel list in module configuration

---
+++ application/controllers/ChannelController.php
@@ -0,0 +1,58 @@
+<?php
+
+/* Icinga NoMa Web | (c) 2023 Icinga GmbH | GPLv2 */
+
+namespace Icinga\Module\Noma\Controllers;
+
+use Icinga\Module\Noma\Common\Database;
+use Icinga\Module\Noma\Forms\ChannelForm;
+use Icinga\Module\Noma\Model\Channel;
+use Icinga\Web\Notification;
+use ipl\Sql\Connection;
+use ipl\Stdlib\Filter;
+use ipl\Web\Compat\CompatController;
+
+class ChannelController extends CompatController
+{
+    /** @var Connection */
+    private $db;
+
+    public function init()
+    {
+        $this->assertPermission('config/modules');
+
+        $this->db = Database::get();
+    }
+
+    public function indexAction()
+    {
+        $channel = Channel::on($this->db);
+        $channelId = $this->params->getRequired('id');
+
+        $channel->filter(Filter::equal('id', $channelId));
+
+        $channel = $channel->first();
+
+        $this->addTitleTab(sprintf(t('Channel: %s'), $channel->name));
+
+        $form = (new ChannelForm($this->db, $channelId))
+            ->populate($channel)
+            ->on(ChannelForm::ON_SUCCESS, function (ChannelForm $form) {
+                if ($form->getPressedSubmitElement()->getName() === 'delete') {
+                    Notification::success(sprintf(
+                        t('Deleted channel "%s" successfully'),
+                        $form->getValue('name')
+                    ));
+                } else {
+                    Notification::success(sprintf(
+                        t('Channel "%s" has successfully been saved'),
+                        $form->getValue('name')
+                    ));
+                }
+
+                $this->redirectNow('__CLOSE__');
+            })->handleRequest($this->getServerRequest());
+
+        $this->addContent($form);
+    }
+}
