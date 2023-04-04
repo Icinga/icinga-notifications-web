598dc082313e1dec1efcea3d0c4de74e4f4043cf
Sukhwinder Dhillon <sukhwinder33445@gmail.com>
2023-04-04 14:39:53 +0200

    Add class `Icons`
    This class provides icon names used in this module
    
    Co-authored-by: Ravi Srinivasa <ravi.srinivasa@icinga.com>

---
+++ library/Noma/Common/Icons.php
@@ -0,0 +1,44 @@
+<?php
+
+/* Icinga NoMa Web | (c) 2023 Icinga GmbH | GPLv2 */
+
+namespace Icinga\Module\Noma\Common;
+
+final class Icons
+{
+    private function __construct()
+    {
+    }
+
+    public const WARNING = 'exclamation-triangle';
+
+    public const OK = 'circle-check';
+
+    public const CRITICAL = 'circle-exclamation';
+
+    public const ERROR = 'circle-xmark';
+
+    public const USER = 'user';
+
+    public const USER_MANAGER = 'user-tie';
+
+    public const CLOSED = 'circle-check';
+
+    public const OPENED = 'sun';
+
+    public const MANAGE = 'circle-check';
+
+    public const UNMANAGE = 'circle-xmark';
+
+    public const UNSUBSCRIBED = 'circle-xmark';
+
+    public const SUBSCRIBED = 'circle-check';
+
+    public const TRIGGERED = 'square-up-right';
+
+    public const PROBLEM = 'circle-exclamation';
+
+    public const RECOVERED = 'heart';
+
+    public const NOTIFIED = 'paper-plane';
+}
