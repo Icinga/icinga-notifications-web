d06d36c3834550615df95610e6fc51f03e130401
Florian Strohmaier <florian.strohmaier@icinga.com>
2023-05-24 15:48:41 +0200

    Make channel form button labels consistent

--- application/forms/ChannelForm.php
+++ application/forms/ChannelForm.php
@@ -156,7 +156,7 @@ class ChannelForm extends CompatForm
             'submit',
             [
                 'label' => $this->channelId === null ?
-                    $this->translate('Add Channel') :
+                    $this->translate('Create Channel') :
                     $this->translate('Save Changes')
             ]
         );
