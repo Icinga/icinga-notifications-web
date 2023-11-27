bb22e6db3a8aa231e4b3ebf2319f5fd2f315b06e
Johannes Meyer <johannes.meyer@icinga.com>
2023-11-27 14:44:49 +0100

    I'm not sure whether to hate phpstan or php

--- phpstan-baseline-by-php-version.php
+++ phpstan-baseline-by-php-version.php
@@ -7,6 +7,12 @@ if (PHP_VERSION_ID >= 80000) {
     $includes[] = __DIR__ . '/phpstan-baseline-7x.neon';
 }
 
+if (PHP_VERSION_ID >= 80200) {
+    $includes[] = __DIR__ . '/phpstan-baseline-8.2+.neon';
+} else {
+    $includes[] = __DIR__ . '/phpstan-baseline-pre-8.2.neon';
+}
+
 return [
     'includes' => $includes
 ];
