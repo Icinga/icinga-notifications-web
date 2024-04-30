5d6e7ab3c118c7222e1bd2e7d9e9dac3a65a40dc
Sukhwinder Dhillon <sukhwinder33445@gmail.com>
2024-04-30 12:27:33 +0200

    ScheduleController: Move setting dialog to `settingsAction`
    IndexAction now shows the calendar
    
    - Add return type hints

--- application/controllers/ScheduleController.php
+++ application/controllers/ScheduleController.php
@@ -4,16 +4,56 @@
 
 namespace Icinga\Module\Notifications\Controllers;
 
+use Icinga\Module\Notifications\Common\Database;
+use Icinga\Module\Notifications\Common\Links;
 use Icinga\Module\Notifications\Forms\EntryForm;
 use Icinga\Module\Notifications\Forms\ScheduleForm;
+use Icinga\Module\Notifications\Model\Schedule;
+use Icinga\Module\Notifications\Widget\Calendar\Controls;
 use Icinga\Module\Notifications\Widget\RecipientSuggestions;
+use Icinga\Module\Notifications\Widget\Schedule as ScheduleWidget;
+use ipl\Html\Form;
 use ipl\Html\Html;
+use ipl\Stdlib\Filter;
 use ipl\Web\Compat\CompatController;
 use ipl\Web\Url;
+use ipl\Web\Widget\ButtonLink;
 
 class ScheduleController extends CompatController
 {
-    public function indexAction()
+    public function indexAction(): void
+    {
+        $id = (int) $this->params->getRequired('id');
+
+        $query = Schedule::on(Database::get())
+            ->filter(Filter::equal('schedule.id', $id));
+
+        /** @var ?Schedule $schedule */
+        $schedule = $query->first();
+        if ($schedule === null) {
+            $this->httpNotFound(t('Schedule not found'));
+        }
+
+        $this->addTitleTab(sprintf(t('Schedule: %s'), $schedule->name));
+
+        $this->controls->addHtml(
+            (new ButtonLink(
+                null,
+                Links::scheduleSettings($id),
+                'cog'
+            ))->openInModal()
+        );
+
+        $this->controls->addAttributes(['class' => 'schedule-controls']);
+
+        $calendarControls = (new Controls())
+            ->setAction(Url::fromRequest()->getAbsoluteUrl())
+            ->handleRequest($this->getServerRequest());
+
+        $this->addContent(new ScheduleWidget($calendarControls, $schedule));
+    }
+
+    public function settingsAction(): void
     {
         $this->setTitle($this->translate('Edit Schedule'));
         $scheduleId = (int) $this->params->getRequired('id');
@@ -25,33 +65,16 @@ class ScheduleController extends CompatController
         $form->setAction($this->getRequest()->getUrl()->getAbsoluteUrl());
         $form->on(ScheduleForm::ON_SUCCESS, function ($form) use ($scheduleId) {
             $form->editSchedule($scheduleId);
-            $this->redirectNow(Url::fromPath('notifications/schedules', ['schedule' => $scheduleId]));
+
+            $this->sendExtraUpdates(['#col1']);
+            $this->redirectNow(Links::schedule($scheduleId));
         });
         $form->on(ScheduleForm::ON_SENT, function ($form) use ($scheduleId) {
             if ($form->hasBeenCancelled()) {
                 $this->redirectNow('__CLOSE__');
             } elseif ($form->hasBeenRemoved()) {
                 $form->removeSchedule($scheduleId);
-                $this->redirectNow(Url::fromPath('notifications/schedules'));
-            }
-        });
 
-        $form->handleRequest($this->getServerRequest());
-
-        $this->addContent($form);
-    }
-
-    public function addAction()
-    {
-        $this->setTitle($this->translate('New Schedule'));
-        $form = new ScheduleForm();
-        $form->setAction($this->getRequest()->getUrl()->getAbsoluteUrl());
-        $form->on(ScheduleForm::ON_SUCCESS, function ($form) {
-            $scheduleId = $form->addSchedule();
-            $this->redirectNow(Url::fromPath('notifications/schedules', ['schedule' => $scheduleId]));
-        });
-        $form->on(ScheduleForm::ON_SENT, function ($form) {
-            if ($form->hasBeenCancelled()) {
                 $this->redirectNow('__CLOSE__');
             }
         });
@@ -61,7 +84,29 @@ class ScheduleController extends CompatController
         $this->addContent($form);
     }
 
-    public function addEntryAction()
+    public function addAction(): void
+    {
+        $this->setTitle($this->translate('New Schedule'));
+        $form = (new ScheduleForm())
+            ->setAction($this->getRequest()->getUrl()->getAbsoluteUrl())
+            ->on(Form::ON_SUCCESS, function (ScheduleForm $form) {
+                $scheduleId = $form->addSchedule();
+
+                $this->sendExtraUpdates(['#col1']);
+                $this->getResponse()->setHeader('X-Icinga-Container', 'col2');
+                $this->redirectNow(Links::schedule($scheduleId));
+            })
+            ->on(Form::ON_SENT, function ($form) {
+                if ($form->hasBeenCancelled()) {
+                    $this->redirectNow('__CLOSE__');
+                }
+            })
+            ->handleRequest($this->getServerRequest());
+
+        $this->addContent($form);
+    }
+
+    public function addEntryAction(): void
     {
         $scheduleId = (int) $this->params->getRequired('schedule');
         $start = $this->params->get('start');
@@ -72,7 +117,7 @@ class ScheduleController extends CompatController
         $form->populate(['when' => ['start' => $start]]);
         $form->on(EntryForm::ON_SUCCESS, function ($form) use ($scheduleId) {
             $form->addEntry($scheduleId);
-            $this->sendExtraUpdates(['#col1']);
+            $this->sendExtraUpdates(['#col2']);
             $this->redirectNow('__CLOSE__');
         });
         $form->on(EntryForm::ON_SENT, function () use ($form) {
@@ -103,7 +148,7 @@ class ScheduleController extends CompatController
         }
     }
 
-    public function editEntryAction()
+    public function editEntryAction(): void
     {
         $entryId = (int) $this->params->getRequired('id');
         $scheduleId = (int) $this->params->getRequired('schedule');
@@ -116,7 +161,7 @@ class ScheduleController extends CompatController
         $form->setSuggestionUrl(Url::fromPath('notifications/schedule/suggest-recipient'));
         $form->on(EntryForm::ON_SUCCESS, function () use ($form, $entryId, $scheduleId) {
             $form->editEntry($scheduleId, $entryId);
-            $this->sendExtraUpdates(['#col1']);
+            $this->sendExtraUpdates(['#col2']);
             $this->redirectNow('__CLOSE__');
         });
         $form->on(EntryForm::ON_SENT, function ($form) use ($entryId, $scheduleId) {
@@ -124,7 +169,7 @@ class ScheduleController extends CompatController
                 $this->redirectNow('__CLOSE__');
             } elseif ($form->hasBeenRemoved()) {
                 $form->removeEntry($scheduleId, $entryId);
-                $this->sendExtraUpdates(['#col1']);
+                $this->sendExtraUpdates(['#col2']);
                 $this->redirectNow('__CLOSE__');
             } elseif (! $form->hasBeenSubmitted()) {
                 foreach ($form->getPartUpdates() as $update) {
@@ -151,7 +196,7 @@ class ScheduleController extends CompatController
         }
     }
 
-    public function suggestRecipientAction()
+    public function suggestRecipientAction(): void
     {
         $suggestions = new RecipientSuggestions();
         $suggestions->forRequest($this->getServerRequest());
