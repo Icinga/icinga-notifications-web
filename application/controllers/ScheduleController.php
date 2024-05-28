2291e8f2bab9b0cb119dd8b1fd4bc0295660e832
Johannes Meyer <johannes.meyer@icinga.com>
2024-05-28 16:57:40 +0200

    ScheduleController: Remove legacy calendar entry actions

--- application/controllers/ScheduleController.php
+++ application/controllers/ScheduleController.php
@@ -6,7 +6,6 @@ namespace Icinga\Module\Notifications\Controllers;
 
 use Icinga\Module\Notifications\Common\Database;
 use Icinga\Module\Notifications\Common\Links;
-use Icinga\Module\Notifications\Forms\EntryForm;
 use Icinga\Module\Notifications\Forms\MoveRotationForm;
 use Icinga\Module\Notifications\Forms\RotationConfigForm;
 use Icinga\Module\Notifications\Forms\ScheduleForm;
@@ -110,48 +109,6 @@ class ScheduleController extends CompatController
         $this->addContent($form);
     }
 
-    public function addEntryAction(): void
-    {
-        $scheduleId = (int) $this->params->getRequired('schedule');
-        $start = $this->params->get('start');
-
-        $form = new EntryForm();
-        $form->setAction($this->getRequest()->getUrl()->getAbsoluteUrl());
-        $form->setSuggestionUrl(Url::fromPath('notifications/schedule/suggest-recipient'));
-        $form->populate(['when' => ['start' => $start]]);
-        $form->on(EntryForm::ON_SUCCESS, function ($form) use ($scheduleId) {
-            $form->addEntry($scheduleId);
-            $this->sendExtraUpdates(['#col2']);
-            $this->redirectNow('__CLOSE__');
-        });
-        $form->on(EntryForm::ON_SENT, function () use ($form) {
-            if ($form->hasBeenCancelled()) {
-                $this->redirectNow('__CLOSE__');
-            } elseif (! $form->hasBeenSubmitted()) {
-                foreach ($form->getPartUpdates() as $update) {
-                    if (! is_array($update)) {
-                        $update = [$update];
-                    }
-
-                    $this->addPart(...$update);
-                }
-            }
-        });
-
-        $form->handleRequest($this->getServerRequest());
-
-        if (empty($this->parts)) {
-            $this->addPart(Html::tag(
-                'div',
-                ['id' => $this->getRequest()->getHeader('X-Icinga-Container')],
-                [
-                    Html::tag('h2', null, $this->translate('Add Entry')),
-                    $form
-                ]
-            ));
-        }
-    }
-
     public function addRotationAction(): void
     {
         $scheduleId = (int) $this->params->getRequired('schedule');
@@ -237,54 +194,6 @@ class ScheduleController extends CompatController
         $form->handleRequest($this->getServerRequest());
 
         $this->addContent($form);
-    }
-
-    public function editEntryAction(): void
-    {
-        $entryId = (int) $this->params->getRequired('id');
-        $scheduleId = (int) $this->params->getRequired('schedule');
-
-        $form = new EntryForm();
-        $form->setShowRemoveButton();
-        $form->loadEntry($scheduleId, $entryId);
-        $form->setSubmitLabel($this->translate('Save Changes'));
-        $form->setAction($this->getRequest()->getUrl()->getAbsoluteUrl());
-        $form->setSuggestionUrl(Url::fromPath('notifications/schedule/suggest-recipient'));
-        $form->on(EntryForm::ON_SUCCESS, function () use ($form, $entryId, $scheduleId) {
-            $form->editEntry($scheduleId, $entryId);
-            $this->sendExtraUpdates(['#col2']);
-            $this->redirectNow('__CLOSE__');
-        });
-        $form->on(EntryForm::ON_SENT, function ($form) use ($entryId, $scheduleId) {
-            if ($form->hasBeenCancelled()) {
-                $this->redirectNow('__CLOSE__');
-            } elseif ($form->hasBeenRemoved()) {
-                $form->removeEntry($scheduleId, $entryId);
-                $this->sendExtraUpdates(['#col2']);
-                $this->redirectNow('__CLOSE__');
-            } elseif (! $form->hasBeenSubmitted()) {
-                foreach ($form->getPartUpdates() as $update) {
-                    if (! is_array($update)) {
-                        $update = [$update];
-                    }
-
-                    $this->addPart(...$update);
-                }
-            }
-        });
-
-        $form->handleRequest($this->getServerRequest());
-
-        if (empty($this->parts)) {
-            $this->addPart(Html::tag(
-                'div',
-                ['id' => $this->getRequest()->getHeader('X-Icinga-Container')],
-                [
-                    Html::tag('h2', null, $this->translate('Edit Entry')),
-                    $form
-                ]
-            ));
-        }
     }
 
     public function suggestRecipientAction(): void
