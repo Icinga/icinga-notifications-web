ec3dd10e2581defb21a5bdbbd089e46d12e540a8
Johannes Meyer <johannes.meyer@icinga.com>
2024-05-28 09:25:19 +0200

    DaemonCommand: Add documentation

--- application/clicommands/DaemonCommand.php
+++ application/clicommands/DaemonCommand.php
@@ -9,6 +9,20 @@ use Icinga\Module\Notifications\Daemon\Daemon;
 
 class DaemonCommand extends Command
 {
+    /**
+     * Run the notifications daemon
+     *
+     * This program allows clients to subscribe to notifications and receive them in real-time on the desktop.
+     *
+     * USAGE:
+     *
+     *     icingacli notifications daemon run [OPTIONS]
+     *
+     * OPTIONS
+     *
+     * --verbose   Enable verbose output
+     * --debug     Enable debug output
+     */
     public function runAction(): void
     {
         Daemon::get();
