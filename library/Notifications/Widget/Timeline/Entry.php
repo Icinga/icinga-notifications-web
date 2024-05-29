9df0285cb81c83e9c0126514227e40bb1953a7e2
Johannes Meyer <johannes.meyer@icinga.com>
2024-05-29 16:05:41 +0200

    Timeline\Entry: Implement `assembleContainer`

--- library/Notifications/Widget/Timeline/Entry.php
+++ library/Notifications/Widget/Timeline/Entry.php
@@ -4,8 +4,12 @@
 
 namespace Icinga\Module\Notifications\Widget\Timeline;
 
+use ipl\Html\Attributes;
 use ipl\Html\BaseHtmlElement;
 use Icinga\Module\Notifications\Widget\TimeGrid;
+use ipl\Html\HtmlElement;
+use ipl\Html\Text;
+use ipl\Web\Widget\Icon;
 
 class Entry extends TimeGrid\Entry
 {
@@ -31,6 +35,37 @@ class Entry extends TimeGrid\Entry
 
     protected function assembleContainer(BaseHtmlElement $container): void
     {
-        // TODO: Implement assembleContainer() method.
+        $container->addHtml(
+            new HtmlElement(
+                'div',
+                Attributes::create(['class' => 'title']),
+                new Icon($this->getMember()->getIcon()),
+                new HtmlElement(
+                    'span',
+                    Attributes::create(['class' => 'name']),
+                    Text::create($this->getMember()->getName())
+                )
+            )
+        );
+
+        $dateType = \IntlDateFormatter::NONE;
+        $timeType = \IntlDateFormatter::SHORT;
+        if (
+            $this->getStart()->diff($this->getEnd())->days > 0
+            || $this->getStart()->format('Y-m-d') !== $this->getEnd()->format('Y-m-d')
+        ) {
+            $dateType = \IntlDateFormatter::SHORT;
+        }
+
+        $formatter = new \IntlDateFormatter(\Locale::getDefault(), $dateType, $timeType);
+
+        $container->addAttributes([
+            'title' => sprintf(
+                $this->translate('%s is available from %s to %s'),
+                $this->getMember()->getName(),
+                $formatter->format($this->getStart()),
+                $formatter->format($this->getEnd())
+            )
+        ]);
     }
 }
