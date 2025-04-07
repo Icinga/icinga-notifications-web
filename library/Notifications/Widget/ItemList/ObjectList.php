b85b8336ff6270e6a945cab655ba4cc246ac0975
Sukhwinder Dhillon <sukhwinder33445@gmail.com>
2025-04-07 09:03:08 +0200

    Introduce IncidentHistoryRenderer
    - Remove obsolete code
    - ObjectList: Add method to disable action-list functionality

--- library/Notifications/Widget/ItemList/ObjectList.php
+++ library/Notifications/Widget/ItemList/ObjectList.php
@@ -19,11 +19,41 @@ use ipl\Web\Widget\ListItem;
  */
 class ObjectList extends ItemList
 {
-    protected $defaultAttributes = ['class' => 'action-list'];
+    /** @var bool Whether the action-list functionality should be disabled */
+    protected $disableActionList = false;
+
+    public function __construct($data, $itemRenderer)
+    {
+        parent::__construct($data, $itemRenderer);
+
+        $this->getAttributes() // TODO(sd): only required for IncidentHistory, find a better solution
+            ->registerAttributeCallback('class', function () {
+                return $this->disableActionList ? null : 'action-list';
+            });
+    }
+
+    /**
+     * Set whether the action-list functionality should be disabled
+     *
+     * @param bool $state
+     *
+     * @return $this
+     */
+    public function disableActionList(bool $state = true): self
+    {
+        $this->disableActionList = $state;
+
+        return $this;
+    }
 
     protected function createListItem(object $data): ListItem
     {
-        return parent::createListItem($data)
-            ->addAttributes(['data-action-item' => true]);
+        $item = parent::createListItem($data);
+
+        if (! $this->disableActionList) {
+            $item->addAttributes(['data-action-item' => true]);
+        }
+
+        return $item;
     }
 }
