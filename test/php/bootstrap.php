92778e1278e77ca78db497499b6358d0b6827c47
Johannes Meyer <johannes.meyer@icinga.com>
2025-08-11 11:29:18 +0200

    Run with:
    ICINGA_NOTIFICATIONS_SCHEMA=/path/to/notifications/schema.sql ICINGAWEB_PATH=/icingaweb2 /usr/share/icinga-php/ipl/vendor/bin/phpunit --bootstrap test/php/bootstrap.php
    
    Note that you will need a database for this, and propagate this via env as well:
    
     * Name              | Description
     * ----------------- | ------------------------
     * *_TESTDB          | The database to use
     * *_TESTDB_HOST     | The server to connect to
     * *_TESTDB_PORT     | The port to connect to
     * *_TESTDB_USER     | The user to connect with
     * *_TESTDB_PASSWORD | The password of the user

---
+++ test/php/bootstrap.php
@@ -0,0 +1,32 @@
+<?php
+
+$webPath = getenv('ICINGAWEB_PATH');
+if ($webPath === false) {
+    echo "ICINGAWEB_PATH environment variable not set\n";
+    exit(1);
+}
+
+$configPath = getenv('ICINGAWEB_CONFIGDIR');
+if (! $configPath) {
+    $configPath = realpath(__DIR__ . '/../config');
+}
+
+system('cp -R ' . $configPath . ' /tmp/config'); // copy
+putenv('ICINGAWEB_CONFIGDIR=/tmp/config');
+system($webPath . '/bin/icingacli module enable notifications');
+
+$pid = pcntl_fork();
+if ($pid == -1) {
+    echo "Could not fork\n";
+    exit(2);
+}
+
+if ($pid) {
+    register_shutdown_function(function() use ($pid) {
+        posix_kill($pid, SIGTERM);
+    });
+
+    require_once $webPath . '/test/php/bootstrap.php';
+} else {
+    pcntl_exec($webPath . '/bin/icingacli', ['web', 'serve']);
+}
