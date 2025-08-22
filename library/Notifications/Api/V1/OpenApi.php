60a3048f464c28c4b5f7c80f5d5b4d78baa39c30
Jan Schuppik <jan.schuppik@icinga.com>
2025-08-22 13:23:37 +0200

    Add: initial openapi docs

--- library/Notifications/Api/V1/OpenApi.php
+++ library/Notifications/Api/V1/OpenApi.php
@@ -7,7 +7,64 @@ use Icinga\Module\Notifications\Common\PsrLogger;
 use Icinga\Util\Environment;
 use Icinga\Web\Response;
 use OpenApi\Generator;
+use OpenApi\Attributes as OA;
 
+#[OA\Schema(
+    schema: 'Url',
+    description: 'A URL used in the API',
+    type: 'string',
+    maxLength: 2048,
+    example: 'example.com',
+)]
+#[OA\Schema(
+    schema: 'Port',
+    description: 'A port number',
+    type: 'integer',
+    format: 'int32',
+    maximum: 65535,
+    minimum: 1,
+)]
+#[OA\Schema(
+    schema: 'Email',
+    description: 'An email address',
+    type: 'string',
+    format: 'email',
+    maxLength: 320,
+)]
+#[OA\Schema(
+    schema: 'ErrorStatus',
+    description: 'status',
+    type: 'string',
+    example: 'error',
+)]
+#[OA\Schema(
+    schema: 'ErrorResponse',
+    description: 'Error response format',
+    properties: [
+        new OA\Property(
+            property: 'status',
+            description: 'Status of the response',
+            type: 'string',
+            example: 'error'
+        ),
+        new OA\Property(
+            property: 'message',
+            description: 'Detailed error message',
+            type: 'string',
+            example: 'An error occurred while processing your request.'
+        )
+    ],
+    type: 'object',
+)]
+#[OA\Schema(
+    schema: 'UUID',
+    title: 'UUID',
+    description: 'An UUID representing',
+    type: 'string',
+    format: 'uuid',
+    maxLength: 36,
+    minLength: 36,
+)]
 class OpenApi extends ApiV1
 {
     public function __construct(
