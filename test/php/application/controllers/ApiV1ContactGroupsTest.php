8ba9bb6a612f11bbf0d9c4d61ba7dc1e7fa6cfc9
Jan Schuppik <jan.schuppik@icinga.com>
2025-09-19 13:11:57 +0200

    Adjust: replace PHP-docs with more descriptive test-names

--- test/php/application/controllers/ApiV1ContactGroupsTest.php
+++ test/php/application/controllers/ApiV1ContactGroupsTest.php
@@ -11,7 +11,7 @@ class ApiV1ContactGroupsTest extends BaseApiV1TestCase
 {
     /**
      * @dataProvider databases
-     * @depends testPostWithValidData
+     * @depends testPostToCreateWithValidData
      */
     public function testGetWithMatchingFilter(): void
     {
@@ -35,7 +35,7 @@ class ApiV1ContactGroupsTest extends BaseApiV1TestCase
 
     /**
      * @dataProvider databases
-     * @depends testPostWithValidData
+     * @depends testPostToCreateWithValidData
      */
     public function testGetEverything(): void
     {
@@ -80,7 +80,7 @@ class ApiV1ContactGroupsTest extends BaseApiV1TestCase
 
     /**
      * @dataProvider databases
-     * @depends testPostWithValidData
+     * @depends testPostToCreateWithValidData
      */
     public function testGetWithAlreadyExistingIdentifier(): void
     {
@@ -116,7 +116,7 @@ class ApiV1ContactGroupsTest extends BaseApiV1TestCase
 
     /**
      * @dataProvider databases
-     * @depends testPostWithValidData
+     * @depends testPostToCreateWithValidData
      */
     public function testGetWithNonMatchingFilter(): void
     {
@@ -134,11 +134,9 @@ class ApiV1ContactGroupsTest extends BaseApiV1TestCase
     }
 
     /**
-     * Create a new contact group with a YAML payload, while declaring the body type as application/json.
-     *
      * @dataProvider databases
      */
-    public function testPostWithInvalidContent(): void
+    public function testPostToCreateWithInvalidContent(): void
     {
         $body = <<<YAML
 ---
@@ -161,11 +159,9 @@ YAML;
     }
 
     /**
-     * Create a new contact group with a YAML payload.
-     *
      * @dataProvider databases
      */
-    public function testPostWithInvalidContentType(): void
+    public function testPostToCreateWithInvalidContentType(): void
     {
         $body = <<<YAML
 ---
@@ -191,11 +187,9 @@ YAML;
     }
 
     /**
-     * Create a new contact group with a valid JSON payload, while providing a filter.
-     *
      * @dataProvider databases
      */
-    public function testPostWithFilter(): void
+    public function testPostToCreateWithFilter(): void
     {
         $response = $this->sendRequest(
             'POST',
@@ -216,11 +210,9 @@ YAML;
     }
 
     /**
-     * Replace a contact group, while providing an unknown identifier.
-     *
      * @dataProvider databases
      */
-    public function testPostWithNewIdentifier(): void
+    public function testPostToReplaceWithNonExistingIdentifier(): void
     {
         $response = $this->sendRequest('POST', 'contactgroups/' . BaseApiV1TestCase::GROUP_UUID, [
             'id' => BaseApiV1TestCase::GROUP_UUID_2,
@@ -234,12 +226,10 @@ YAML;
     }
 
     /**
-     * Replace a contact group, while providing the same identifier in the Request-URI and the JSON payload.
-     *
      * @dataProvider databases
-     * @depends testPostWithValidData
+     * @depends testPostToCreateWithValidData
      */
-    public function testPostWithAlreadyExistingIdentifierAndIndifferentPayloadId(): void
+    public function testPostToReplaceWithAlreadyExistingIdentifierAndIndifferentPayloadId(): void
     {
         $this->sendRequest('POST', 'contactgroups', [
             'id' => BaseApiV1TestCase::GROUP_UUID,
@@ -262,12 +252,10 @@ YAML;
     }
 
     /**
-     * Replace a contact with an id which already exists
-     *
      * @dataProvider databases
-     * @depends testPostWithValidData
+     * @depends testPostToCreateWithValidData
      */
-    public function testPostWithAlreadyExistingIdentifierAndExistingPayloadId(): void
+    public function testPostToReplaceWithAlreadyExistingIdentifierAndExistingPayloadId(): void
     {
         $this->sendRequest('POST', 'contactgroups', [
             'id' => BaseApiV1TestCase::GROUP_UUID,
@@ -293,12 +281,10 @@ YAML;
     }
 
     /**
-     * Replace a contact group while providing a new identifier in the JSON payload.
-     *
      * @dataProvider databases
-     * @depends testPostWithValidData
+     * @depends testPostToCreateWithValidData
      */
-    public function testPostWithAlreadyExistingIdentifierAndValidData(): void
+    public function testPostToReplaceWithAlreadyExistingIdentifierAndValidData(): void
     {
         $this->sendRequest('POST', 'contactgroups', [
             'id' => BaseApiV1TestCase::GROUP_UUID,
@@ -322,12 +308,10 @@ YAML;
     }
 
     /**
-     * Create a new contact group with a valid JSON payload, while providing an already existing Payload id.
-     *
      * @dataProvider databases
-     * @depends testPostWithValidData
+     * @depends testPostToCreateWithValidData
      */
-    public function testPostWithAlreadyExistingPayloadId(): void
+    public function testPostToCreateWithAlreadyExistingPayloadId(): void
     {
         $this->sendRequest('POST', 'contactgroups', [
             'id' => BaseApiV1TestCase::GROUP_UUID,
@@ -347,11 +331,9 @@ YAML;
     }
 
     /**
-     * Create a new contact group with a valid JSON payload.
-     *
      * @dataProvider databases
      */
-    public function testPostWithValidData(): void
+    public function testPostToCreateWithValidData(): void
     {
         $response = $this->sendRequest('POST', 'contacts', [
             'id' => '0817d973-398e-41d7-9ef2-61cdb7ef41a1',
@@ -389,11 +371,9 @@ YAML;
     }
 
     /**
-     * Create a new contact group with an incorrect JSON payload.
-     *
      * @dataProvider databases
      */
-    public function testPostWithInvalidData(): void
+    public function testPostToCreateWithInvalidData(): void
     {
         $expected = $this->jsonEncodeError(
             'Invalid request body: the fields id and name must be present and of type string'
@@ -435,12 +415,10 @@ YAML;
     }
 
     /**
-     * Replace a contact group with an incorrect JSON payload.
-     *
      * @dataProvider databases
-     * @depends testPostWithValidData
+     * @depends testPostToCreateWithValidData
      */
-    public function testPostWithAlreadyExistingIdentifierAndInvalidData(): void
+    public function testPostToReplaceWithAlreadyExistingIdentifierAndInvalidData(): void
     {
         $this->sendRequest('POST', 'contactgroups', [
             'id' => BaseApiV1TestCase::GROUP_UUID,
@@ -474,11 +452,9 @@ YAML;
     }
 
     /**
-     * Replace a contact group with a YAML payload, while declaring the body type as application/json.
-     *
      * @dataProvider databases
      */
-    public function testPutWithInvalidContent(): void
+    public function testPutToUpdateWithInvalidContent(): void
     {
         $body = <<<YAML
 ---
@@ -501,11 +477,9 @@ YAML;
     }
 
     /**
-     * Replace a contact group with a YAML payload, while declaring the body type as application/json.
-     *
      * @dataProvider databases
      */
-    public function testPutWithInvalidContentType(): void
+    public function testPutToUpdateWithInvalidContentType(): void
     {
         $body = <<<YAML
 ---
@@ -531,11 +505,9 @@ YAML;
     }
 
     /**
-     * Replace a contact group with a valid JSON payload, while providing a filter.
-     *
      * @dataProvider databases
      */
-    public function testPutWithFilter(): void
+    public function testPutToUpdateWithFilter(): void
     {
         $response = $this->sendRequest('PUT', 'contactgroups?id=' . BaseApiV1TestCase::GROUP_UUID, [
                 'id' => BaseApiV1TestCase::GROUP_UUID,
@@ -552,11 +524,9 @@ YAML;
     }
 
     /**
-     * Replace a contact group, while omitting the identifier in the JSON payload.
-     *
      * @dataProvider databases
      */
-    public function testPutWithoutIdentifier(): void
+    public function testPutToUpdateWithoutIdentifier(): void
     {
         $response = $this->sendRequest('PUT', 'contactgroups', [
             'id' => BaseApiV1TestCase::GROUP_UUID,
@@ -570,13 +540,10 @@ YAML;
     }
 
     /**
-     * Replace a contact group with the same identifier in the Request-URI
-     * and the JSON payload, while omitting required fields.
-     *
      * @dataProvider databases
-     * @depends testPostWithValidData
+     * @depends testPostToCreateWithValidData
      */
-    public function testPutWithAlreadyExistingIdentifierAndMissingRequiredFields(): void
+    public function testPutToUpdateWithAlreadyExistingIdentifierAndMissingRequiredFields(): void
     {
         $this->sendRequest('POST', 'contactgroups', [
             'id' => BaseApiV1TestCase::GROUP_UUID,
@@ -610,12 +577,10 @@ YAML;
     }
 
     /**
-     * Replace a contact group with a different identifier in the Request-URI and the JSON payload.
-     *
      * @dataProvider databases
-     * @depends testPostWithValidData
+     * @depends testPostToCreateWithValidData
      */
-    public function testPutWithAlreadyExistingIdentifierAndDifferentPayloadId(): void
+    public function testPutToUpdateWithAlreadyExistingIdentifierAndDifferentPayloadId(): void
     {
         $this->sendRequest('POST', 'contactgroups', [
             'id' => BaseApiV1TestCase::GROUP_UUID,
@@ -636,11 +601,9 @@ YAML;
     }
 
     /**
-     * Create a new contact group with a valid JSON payload, while providing a new identifier.
-     *
      * @dataProvider databases
      */
-    public function testPutWithNewIdentifierAndValidData(): void
+    public function testPutToCreateWithNewIdentifierAndValidData(): void
     {
         $response = $this->sendRequest('POST', 'contacts', [
             'id' => BaseApiV1TestCase::CONTACT_UUID,
@@ -665,12 +628,10 @@ YAML;
     }
 
     /**
-     * Replace a contact group with the same identifier in the Request-URI and the JSON payload.
-     *
      * @dataProvider databases
-     * @depends testPostWithValidData
+     * @depends testPostToCreateWithValidData
      */
-    public function testPutWithAlreadyExistingIdentifierAndValidData(): void
+    public function testPutToUpdateWithAlreadyExistingIdentifierAndValidData(): void
     {
         $this->sendRequest('POST', 'contactgroups', [
             'id' => BaseApiV1TestCase::GROUP_UUID,
@@ -690,11 +651,9 @@ YAML;
     }
 
     /**
-     * Create a new contact group with incorrect JSON payload, while providing a new identifier.
-     *
      * @dataProvider databases
      */
-    public function testPutWithNewIdentifierAndInvalidData(): void
+    public function testPutToCreateWithNewIdentifierAndInvalidData(): void
     {
         // different id
         $response = $this->sendRequest('PUT', 'contactgroups/' . BaseApiV1TestCase::GROUP_UUID, [
@@ -723,11 +682,9 @@ YAML;
     }
 
     /**
-     * Create a new contact group with missing optional fields in the JSON payload, while providing a new identifier.
-     *
      * @dataProvider databases
      */
-    public function testPutWithNewIdentifierAndValidOptionalData(): void
+    public function testPutToCreateWithNewIdentifierAndValidOptionalData(): void
     {
         $response = $this->sendRequest('PUT', 'contactgroups/' . BaseApiV1TestCase::GROUP_UUID, [
             'id' => BaseApiV1TestCase::GROUP_UUID,
@@ -744,11 +701,9 @@ YAML;
     }
 
     /**
-     * Create a new contact group with missing required fields in the JSON payload, while providing a new identifier.
-     *
      * @dataProvider databases
      */
-    public function testPutWithNewIdentifierAndMissingRequiredFields(): void
+    public function testPutToCreateWithNewIdentifierAndMissingRequiredFields(): void
     {
         $expected = $this->jsonEncodeError(
             'Invalid request body: the fields id and name must be present and of type string'
@@ -800,7 +755,7 @@ YAML;
 
     /**
      * @dataProvider databases
-     * @depends testPostWithValidData
+     * @depends testPostToCreateWithValidData
      */
     public function testDeleteWithAlreadyExistingIdentifier(): void
     {
