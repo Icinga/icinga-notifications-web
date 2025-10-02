ed9a431e08d4b7a0e8944f4b74ca7e6911ff1d2a
Johannes Meyer <johannes.meyer@icinga.com>
2025-10-02 17:31:06 +0200

    FutureEntry: Simplify implementation
    If it's not an `.entry`, that's fine. But making the
    child one, is not. Only children of the overlay must
    be entries. If style rules apply, copy them over.

--- library/Notifications/Widget/Timeline/FutureEntry.php
+++ library/Notifications/Widget/Timeline/FutureEntry.php
@@ -14,16 +14,16 @@ use ipl\Web\Widget\Icon;
  * FutureEntry
  *
  * Visualize a future entry of the rotation
- *
- * @extends Entry<0>
  */
 class FutureEntry extends Entry
 {
+    protected $defaultAttributes = ['class' => 'future-entry'];
+
+    protected $continuationType = Entry::TO_NEXT_GRID;
+
     public function __construct()
     {
         parent::__construct(0);
-
-        $this->setContinuationType(Entry::TO_NEXT_GRID);
     }
 
     public function getColor(int $transparency): string
@@ -34,17 +34,12 @@ class FutureEntry extends Entry
 
     protected function assembleContainer(BaseHtmlElement $container): void
     {
-        $futureBadge = new HtmlElement(
+        $container->addHtml(new HtmlElement(
             'div',
             new Attributes([
-                'title' => $this->translate('Rotation starts in the future'),
-                $container->getAttributes()->get('class')
+                'title' => $this->translate('Rotation starts in the future')
             ]),
             new Icon('angle-right')
-        );
-
-        $container
-            ->setAttribute('class', 'future-entry') // override the default class
-            ->addHtml($futureBadge);
+        ));
     }
 }
