7f2606c5836201f76c0e26618a56cafa6cf61e07
Johannes Meyer <johannes.meyer@icinga.com>
2025-10-23 11:43:20 +0200

    Source: Let integrations provide their own icon

--- library/Notifications/Model/Source.php
+++ library/Notifications/Model/Source.php
@@ -5,6 +5,9 @@
 namespace Icinga\Module\Notifications\Model;
 
 use DateTime;
+use Icinga\Application\Hook;
+use Icinga\Application\Logger;
+use Icinga\Module\Notifications\Hook\V1\SourceHook;
 use ipl\Orm\Behavior\BoolCast;
 use ipl\Orm\Behavior\MillisecondTimestamp;
 use ipl\Orm\Behaviors;
@@ -12,6 +15,7 @@ use ipl\Orm\Model;
 use ipl\Orm\Query;
 use ipl\Orm\Relations;
 use ipl\Web\Widget\Icon;
+use Throwable;
 
 /**
  * @property int $id The primary key
@@ -26,6 +30,9 @@ use ipl\Web\Widget\Icon;
  */
 class Source extends Model
 {
+    /** @var array<string, Icon> */
+    private static array $icons = [];
+
     public function getTableName(): string
     {
         return 'source';
@@ -85,7 +92,35 @@ class Source extends Model
      */
     public function getIcon(): Icon
     {
-        // TODO: Let the hook deliver the icon
-        return new Icon('share-nodes');
+        if (array_key_exists($this->type, self::$icons)) {
+            return self::$icons[$this->type];
+        }
+
+        // Fallback, in case an integration is inactive or missing
+        $icon = new Icon('share-nodes');
+
+        foreach (Hook::all('Notifications/v1/Source') as $hook) {
+            /** @var SourceHook $hook */
+            try {
+                if ($hook->getSourceType() === $this->type) {
+                    $icon = $hook->getSourceIcon();
+
+                    break;
+                }
+            } catch (Throwable $e) {
+                Logger::error(
+                    'Failed to retrieve source icon for source type "%s": %s',
+                    $this->type,
+                    $e
+                );
+
+                break;
+            }
+        }
+
+        // Cache icons, they don't change during a session but are required multiple times
+        self::$icons[$this->type] = $icon;
+
+        return $icon;
     }
 }
