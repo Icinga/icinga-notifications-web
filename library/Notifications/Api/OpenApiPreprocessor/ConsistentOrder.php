1860d9cd57184e3245926dfceadc2246b3f2593c
Johannes Meyer <johannes.meyer@icinga.com>
2025-11-19 10:27:47 +0100

    Ensure a consistent order of entries in the api description
    It's a rather large file and so I'd like to have as less
    noise in diffs as possible when re-generating it.

---
+++ library/Notifications/Api/OpenApiPreprocessor/ConsistentOrder.php
@@ -0,0 +1,27 @@
+<?php
+
+/* Icinga Notifications Web | (c) 2025 Icinga GmbH | GPLv2 */
+
+namespace Icinga\Module\Notifications\Api\OpenApiPreprocessor;
+
+use OpenApi\Analysis;
+
+class ConsistentOrder
+{
+    public function __invoke(Analysis $analysis): void
+    {
+        if (is_object($analysis->openapi->components) && is_iterable($analysis->openapi->components->schemas)) {
+            usort($analysis->openapi->components->schemas, function ($a, $b) {
+                return $a->schema <=> $b->schema;
+            });
+        }
+
+        usort($analysis->openapi->paths, function ($a, $b) {
+            return $a->path <=> $b->path;
+        });
+
+        usort($analysis->openapi->tags, function ($a, $b) {
+            return $a->name <=> $b->name;
+        });
+    }
+}
