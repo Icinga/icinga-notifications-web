7ba3d085bb805c819f2e74da6476e5515eef6886
Eric Lippmann <eric.lippmann@icinga.com>
2026-02-23 23:34:14 +0100

    License source files as GPL-3.0-or-later
    Add SPDX license headers and mark source files as GPL-3.0-or-later to
    preserve the option to relicense under later GPL versions.

--- library/Notifications/Widget/TimeGrid/EntryProvider.php
+++ library/Notifications/Widget/TimeGrid/EntryProvider.php
@@ -1,5 +1,8 @@
 <?php
 
+// SPDX-FileCopyrightText: 2024 Icinga GmbH <https://icinga.com>
+// SPDX-License-Identifier: GPL-3.0-or-later
+
 namespace Icinga\Module\Notifications\Widget\TimeGrid;
 
 use ipl\Web\Url;
