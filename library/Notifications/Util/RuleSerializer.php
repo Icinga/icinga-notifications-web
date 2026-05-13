f9259a1ada38a9670e165de8029349b68d6703b9
Bastian Lederer <bastian.lederer@icinga.com>
2026-05-13 14:38:38 +0200

    Introduce `RuleSerializer`

---
+++ library/Notifications/Util/RuleSerializer.php
@@ -0,0 +1,121 @@
+<?php
+
+// SPDX-FileCopyrightText: 2026 Icinga GmbH <https://icinga.com>
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+namespace Icinga\Module\Notifications\Util;
+
+use Icinga\Exception\Json\JsonEncodeException;
+use Icinga\Util\Json;
+use ipl\Stdlib\Filter;
+use ipl\Stdlib\Filter\Chain;
+use ipl\Web\Filter\Renderer;
+
+class RuleSerializer
+{
+    /** @var Filter\Condition|Filter\Chain */
+    protected Filter\Rule $filter;
+
+    /**
+     * Create an object that can be used to serialize a rule to JSON
+     *
+     * @param Filter\Rule $filter
+     */
+    public function __construct(Filter\Rule $filter)
+    {
+        $this->filter = $filter;
+    }
+
+    /**
+     * Serialize the filter as Json
+     *
+     * @return string
+     *
+     * @throws JsonEncodeException
+     */
+    public function getJson(): string
+    {
+        $result = ['qs' => (new Renderer($this->filter))->render()];
+        if ($this->filter instanceof Filter\Chain) {
+            $result['ast'] = $this->serializeChain($this->filter);
+        } else {
+            $result['ast'] = $this->serializeCondition($this->filter);
+        }
+
+        return Json::encode($result);
+    }
+
+    /**
+     * Create an array with keys `op` and `rules` from a chain
+     *
+     * @param Chain $chain
+     *
+     * @return array{op: string, rules: list<array<string, mixed>>}
+     */
+    protected function serializeChain(Chain $chain): array
+    {
+        $result = [
+            'op' => match (true) {
+                $chain instanceof Filter\All => '&',
+                $chain instanceof Filter\None => '!',
+                $chain instanceof Filter\Any => '|'
+            }
+        ];
+
+        $rules = [];
+        foreach ($chain as $rule) {
+            if ($rule instanceof Chain) {
+                $rules[] = $this->serializeChain($rule);
+            } else {
+                $rules[] = $this->serializeCondition($rule);
+            }
+        }
+
+        $result['rules'] = $rules;
+        return $result;
+    }
+
+    /**
+     * Create an array with the keys `op`, `attributes` and either `value` or `regex`
+     *
+     * @param Filter\Condition $condition
+     *
+     * @return array{op: string, attributes: list<string>, value: string}
+     *       | array{op: string, attributes: list<string>, regex: string}
+     */
+    protected function serializeCondition(Filter\Condition $condition): array
+    {
+        $op = match (true) {
+            $condition instanceof Filter\Unlike, $condition instanceof Filter\Unequal => '!=',
+            $condition instanceof Filter\Like, $condition instanceof Filter\Equal => '=',
+            $condition instanceof Filter\GreaterThan => '>',
+            $condition instanceof Filter\LessThan => '<',
+            $condition instanceof Filter\GreaterThanOrEqual => '>=',
+            $condition instanceof Filter\LessThanOrEqual => '<=',
+        };
+
+        $value = $condition instanceof Filter\Like || $condition instanceof Filter\Unlike
+            ? ['regex' => $this->preprocessValue($condition->getValue())]
+            : ['value' => $condition->getValue()];
+
+        return [
+            'op' => $op,
+            'attributes' => $condition->metaData()->get('jsonPaths'),
+            ...$value,
+        ];
+    }
+
+    /**
+     * Get the preprocessed value of a condition
+     *
+     * Creates a regex for Like and Unlike rules
+     *
+     * @param string $value
+     *
+     * @return string
+     */
+    protected function preprocessValue(string $value): string
+    {
+        return '^' . str_replace('\*', '.*', preg_quote($value)) . '$';
+    }
+}
