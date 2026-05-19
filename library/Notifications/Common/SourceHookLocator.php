94b51f8f251b4c4e5a044ec5e94ac2fcbde1f6bd
Bastian Lederer <bastian.lederer@icinga.com>
2026-05-19 14:05:20 +0200

    `SourceHookLocator`: Use double backslashes

--- library/Notifications/Common/SourceHookLocator.php
+++ library/Notifications/Common/SourceHookLocator.php
@@ -28,11 +28,11 @@ class SourceHookLocator
     {
         $name = ucfirst(Str::camel($type));
 
-        $alias = 'Icinga\Module\Notifications\Hook\V2\\' . $name . 'SourceHook';
+        $alias = 'Icinga\\Module\\Notifications\\Hook\\V2\\' . $name . 'SourceHook';
         if (! interface_exists($alias)) {
             class_alias(SourceHook::class, $alias);
         }
 
-        return Hook::first('Notifications/v2/' . $name . 'Source');
+        return Hook::first('Notifications\\V2\\' . $name . 'Source');
     }
 }
