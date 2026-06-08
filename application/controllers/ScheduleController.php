9ec4d722e7a424f07a53809c6675faefdbcdf38a
Sukhwinder Dhillon <sukhwinder.dhillon@icinga.com>
2026-06-08 11:13:16 +0200

    `ScheduleController`: Use `edit` icon for schedule settings button

--- application/controllers/ScheduleController.php
+++ application/controllers/ScheduleController.php
@@ -57,7 +57,7 @@ class ScheduleController extends CompatController
             (new ButtonLink(
                 null,
                 Links::scheduleSettings($id),
-                'cog'
+                'edit'
             ))->openInModal()
         );
 
