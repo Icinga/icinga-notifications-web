0dc0529d093e34b6a1e1ec6ac05194de655a00c5
Sukhwinder Dhillon <sukhwinder.dhillon@icinga.com>
2026-06-11 14:38:13 +0200

    Introduce initial setup

---
+++ application/controllers/IndexController.php
@@ -0,0 +1,26 @@
+<?php
+
+// SPDX-FileCopyrightText: 2026 Icinga GmbH <https://icinga.com>
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+namespace Icinga\Module\Notifications\Controllers;
+
+use Icinga\Module\Notifications\Widget\InitialSetup;
+use ipl\Web\Compat\CompatController;
+use ipl\Web\Url;
+
+class IndexController extends CompatController
+{
+    public function indexAction(): void
+    {
+        $this->addTitleTab($this->translate('Initial Setup'));
+
+        $setup = new InitialSetup();
+
+        $this->addContent($setup);
+
+        if ($setup->ensureAssembled()->isFinished()) {
+            $this->redirectNow(Url::fromPath('navigation/dashboard', ['name' => 'notifications']));
+        }
+    }
+}
