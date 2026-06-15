e4e57abb42f051816971924b21b8734879128743
Sukhwinder Dhillon <sukhwinder.dhillon@icinga.com>
2026-06-15 15:27:52 +0200

    Introduce class `SourceRepository`
    - Replace class `SourceFormTest` with `SourceRepositoryTest`

--- test/php/library/Notifications/Repository/SourceRepositoryTest.php
+++ test/php/library/Notifications/Repository/SourceRepositoryTest.php
@@ -1,20 +1,18 @@
 <?php
 
-// SPDX-FileCopyrightText: 2024 Icinga GmbH <https://icinga.com>
+// SPDX-FileCopyrightText: 2026 Icinga GmbH <https://icinga.com>
 // SPDX-License-Identifier: GPL-3.0-or-later
 
-namespace Tests\Icinga\Module\Notifications\Forms;
-
-use Icinga\Module\Notifications\Forms\SourceForm;
+use Icinga\Module\Notifications\Repository\SourceRepository;
 use PHPUnit\Framework\TestCase;
 
-class SourceFormTest extends TestCase
+class SourceRepositoryTest extends TestCase
 {
     public function testWhetherTheUsedHashAlgorithmIsStillTheDefault()
     {
         $this->assertSame(
             PASSWORD_DEFAULT,
-            SourceForm::HASH_ALGORITHM,
+            SourceRepository::HASH_ALGORITHM,
             'PHP\'s default password hash algorithm changed. Consider adding support for it'
         );
     }
