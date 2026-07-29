9a40081d9e7e6f748c20c79730dfdca7c868f8ab
Johannes Meyer <johannes.meyer@icinga.com>
2026-07-29 16:27:03 +0200

    Handle soft-deleted sources when showing incidents

--- library/Notifications/Widget/Detail/IncidentDetail.php
+++ library/Notifications/Widget/Detail/IncidentDetail.php
@@ -21,7 +21,6 @@ use ipl\Html\HtmlElement;
 use ipl\Html\Text;
 use ipl\Html\ValidHtml;
 use ipl\I18n\Translation;
-use ipl\Stdlib\Filter;
 use ipl\Web\Layout\MinimalItemLayout;
 use ipl\Web\Url;
 use ipl\Web\Widget\CopyToClipboard;
@@ -68,7 +67,7 @@ class IncidentDetail extends BaseHtmlElement
             || ! $this->getAuth()->hasPermission('notifications/config/contacts');
 
         return [
-            Html::tag('h2', t('Subscribers')),
+            Html::tag('h2', $this->translate('Subscribers')),
             (new ObjectList($contacts, (new IncidentContactRenderer())->disableContactLink($disableContactLink)))
                 ->setItemLayoutClass(MinimalItemLayout::class)
                 ->setDetailActionsDisabled($disableContactLink)
@@ -79,7 +78,9 @@ class IncidentDetail extends BaseHtmlElement
     protected function createRelatedObject(): array
     {
         $object = $this->incident->object;
-        $hook = SourceHookLocator::forType($object->source->type);
+        $hook = isset($object->source->type)
+            ? SourceHookLocator::forType($object->source->type)
+            : null;
 
         $objectUrl = $hook?->createObjectLink($object->id_tags);
         if ($objectUrl === null) {
@@ -100,7 +101,7 @@ class IncidentDetail extends BaseHtmlElement
         }
 
         return [
-            new HtmlElement('h2', null, Text::create(t('Related Object'))),
+            new HtmlElement('h2', null, Text::create($this->translate('Related Object'))),
             $objectUrl
         ];
     }
@@ -151,7 +152,7 @@ class IncidentDetail extends BaseHtmlElement
             ]);
 
         return [
-            Html::tag('h2', t('Incident History')),
+            Html::tag('h2', $this->translate('Incident History')),
             (new ObjectList($query, new IncidentHistoryRenderer()))
                 ->setItemLayoutClass(MinimalItemLayout::class)
                 ->setDetailActionsDisabled()
@@ -161,11 +162,18 @@ class IncidentDetail extends BaseHtmlElement
     /** @return ValidHtml[] */
     protected function createSource(): array
     {
+        if (! isset($this->incident->object->source->name)) {
+            return [
+                Html::tag('h2', $this->translate('Event Source')),
+                new EmptyState($this->translate('No source information available'))
+            ];
+        }
+
         $list = new HtmlElement('ul', Attributes::create(['class' => 'source-list']));
         $list->addHtml(new HtmlElement('li', null, new EventSourceBadge($this->incident->object->source)));
 
         return [
-            Html::tag('h2', t('Event Source')),
+            Html::tag('h2', $this->translate('Event Source')),
             $list
         ];
     }
