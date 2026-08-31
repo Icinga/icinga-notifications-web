b2ac761cc2e875564164babf5f26c5dfa5fa449e
Bastian Lederer <bastian.lederer@icinga.com>
2026-08-31 07:58:27 +0200

    Rework `Incidents` and `Incident` api
    Add 3 separate factories to `Incidents`.
    
    `matchAll()` and `getAll()` aaccept an iterable where each element
    is a set of tags, all incidents matching any of the tag sets are yielded.
    
    In the case of `getAll()` exact matches are required, `matchAll()` allows
    partial matches as well.
    
    `get()` expects the full tags of a single object, and creates an `Incident`
    from a query instead of a `Model`. The query is run lazily, if it does not return
    an `Inciednt` an `IncidentNotFoundException` is thrown.

---
+++ library/Notifications/Integrations/Exception/IncidentNotFoundException.php
@@ -0,0 +1,12 @@
+<?php
+
+// SPDX-FileCopyrightText: 2026 Icinga GmbH <https://icinga.com>
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+namespace Icinga\Module\Notifications\Integrations\Exception;
+
+use Exception;
+
+class IncidentNotFoundException extends Exception
+{
+}
